513a1d9cfa10325543e9a987875655609febae2b
AndreBarr <abarr195@fiu.edu>
2020-08-23 21:04:23 -0400

    Basic function work

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 const express = require("express");
-const { response } = require("express");
+const cors = require("cors");
+const monk = require("monk");
 
 const app = express();
+
+const db = monk("localhost/fitjournal");
+const exercises = db.get("exercises");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "HelloTest",
+  });
+});
+
+function isValidExercise(exercise) {
+  return (
+    exercise.type &&
+    exercise.type.toString().trim() !== "" &&
+    exercise.sets &&
+    exercise.sets.toString().trim() !== "" &&
+    exercise.reps &&
+    exercise.reps.toString().trim() !== ""
+  );
+}
+
+app.post("/exercise", (req, res) => {
+  if (isValidExercise(req.body)) {
+    // insert into db...
+    const exercise = {
+      type: req.body.type.toString(),
+      sets: req.body.sets.toString(),
+      reps: req.body.reps.toString(),
+      created: new Date(),
+    };
+
+    exercises.insert(exercise).then((createdExercise) => {
+      res.json(createdExercise);
+    });
+  } else {
+    res.status(422);
+    res.json({
+      message: "Exercise requires type, sets and reps!",
+    });
+  }
+});
 
 app.listen(5000, () => {
   console.log("Listening on http://localhost:5000");
